4ba73e75be5323ccc2abc88da4665fc3d0baebf6
CodeOn-ArK <aarya.codeeka@gmail.com>
2021-04-18 20:18:14 +0530

    Backup SRAM added

--- 14_BackupSRAM/Core/Src/main.c
+++ 14_BackupSRAM/Core/Src/main.c
@@ -5,6 +5,12 @@
  *      Author: ark
  */
 
+/*
+ * Prog to measure the current consumption in STANDBY mode
+ *
+ * - BKUP SRAM on && RTC off
+ * - Observe the contents of BKUP SRAM before && after STANDBY mode
+ */
 
 #include "main.h"
 
