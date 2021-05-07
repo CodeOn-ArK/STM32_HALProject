c75ccaa528cae5446ca247bdfcfb79e42aa197af
CodeOn-ArK <aarya.codeeka@gmail.com>
2021-05-07 19:17:04 +0530

    SRAM -> UART DMA

---
+++ 22_SRAM_UART_DMA/Core/Src/data_stream.c
@@ -0,0 +1,5 @@
+#include<stdint.h>
+
+uint8_t stream[] = " Each input or output url can, in principle, contain any number of streams of different types (video/audio/subtitle/attachment/data). The allowed number and/or types of streams";
+
+uint32_t data_len = sizeof(stream);
